e308a62ecb666a534e2cefc6267b3ece272f8a9b
Jayant <98017109+goyal1510@users.noreply.github.com>
2024-11-08 09:17:49 +0530

    Update app.js parul

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = (c.width = window.innerWidth),
   hw = w / 2,
   hh = h / 2,
   opts = {
-    strings: ["HAPPY", "BIRTHDAY", "YASHIKA DI ❤️"],
+    strings: ["HAPPY", "BIRTHDAY", "PARUL DI ❤️"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
